0b13e577d6fd5b6970ab3f45d79308cb2b85f80e
Victor Dantas <99653338+victormcdantas02@users.noreply.github.com>
2026-04-30 20:29:03 -0300

    Update loja.sql

--- loja.sql
+++ loja.sql
@@ -2,9 +2,34 @@ CREATE DATABASE loja;
 USE loja;
 
 CREATE TABLE cliente(
-ID INT PRIMARY KEY AUTO_INCREMENT,
+id_cliente INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
-idade INT NOT NULL;
-sexo BOOLEAN;
-data_nascimento DATE;
+idade INT NOT NULL,
+sexo BOOLEAN,
+data_nascimento DATE,
 );
+
+CREATE TABLE vendedor(
+  id_vendedor INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(30) NOT NULL,
+  causa_social,
+  tipo,
+  nota_media DECIMAL,
+  );
+
+CREATE TABLE produto(
+ id_produto INT PRIMARY KEY AUTO_INCREMENT,
+ nome VARCHAR(30) NOT NULL,
+ descricao TEXT,
+ quantidade_estoque,
+ valor,
+ observacoes TEXT,
+ );
+
+CREATE TABLE transportadora(
+    id_transportadora INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    cidade,
+  );
+
+
